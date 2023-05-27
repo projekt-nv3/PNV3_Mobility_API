bea64ee7ca03eddbd0af5ff0ea0ce81e1914a2fe
Elevenv <vitthaljadhav1420@gmail.com>
2023-05-28 04:28:05 +0530

    BREAK system added

--- PNV3_servo_BLDC.ino
+++ PNV3_servo_BLDC.ino
@@ -2,37 +2,62 @@
 #define NORMAL 90
 #define DISENGAGE 98
 #define REVERSE 70
+#define NO_BREAK 145
+#define BREAK 95
 
 Servo ph_shift;
 Servo esc;
+Servo Break;
+
 
 void setup() {
-  // put your setup code here, to run once:4
   Serial.begin(9600);
   
-  Serial.println("ATTACHED");
-  ph_shift.attach(10);  // attaches the servo on pin 10 to the servo object
+  ph_shift.attach(10); 
   ph_shift.write(90);
-  esc.attach(9);
-  // delay(5000);
-  Serial.println("INITIALIZED");
   
+  Break.attach(11);
+  Break.write(NO_BREAK);
+ 
+  esc.attach(9);
   esc.writeMicroseconds(1400);
   delay(3000);
-  Serial.println("INITIALIZED");
-  
-  // esc.writeMicroseconds(1600);
-  // delay(3000);
+
 }
 
 void loop() {
   bool reverse = false;
 
   for(int throttle=1410; throttle<=1500; throttle++) {
-    Serial.println(throttle);
+    // Serial.println(throttle);
     esc.writeMicroseconds(throttle);
     delay(100);
-  
+
+    // if(throttle==1460){
+    //   Serial.println("1460");
+    //   esc.writeMicroseconds(0);
+    //   delay(2000);
+    // }
+
+    // if(throttle==1499){
+    //   esc.writeMicroseconds(0);
+    //   ph_shift.write(DISENGAGE);
+    //   for(int angle = NO_BREAK; angle > BREAK; angle-=15)    
+    //     {                                
+    //       Break.write(angle);           
+    //       delay(100);       
+    //     }
+    //     delay(1000);
+    //   ph_shift.write(NORMAL);
+    //   for(int angle = BREAK; angle < NO_BREAK; angle+=15)  
+    //     {                                  
+    //       Break.write(angle);               
+    //       delay(100);                   
+    //     }
+    // }
+
+     
+
     if (Serial.available() > 0){
       String msg = Serial.readString();
       int num;
@@ -41,8 +66,18 @@ void loop() {
       if(!reverse) {
         esc.writeMicroseconds(0);
         ph_shift.write(DISENGAGE);
+        for(int angle = NO_BREAK; angle > BREAK; angle-=15)    
+        {                                
+          Break.write(angle);           
+          delay(100);       
+        } 
         delay(1000);
 
+        for(int angle = BREAK; angle < NO_BREAK; angle+=15)  
+        {                                  
+          Break.write(angle);               
+          delay(100);                   
+        } 
         ph_shift.write(REVERSE);
         throttle=1410;
         reverse = true;
@@ -52,21 +87,23 @@ void loop() {
         esc.writeMicroseconds(0);
         delay(400);
         ph_shift.write(DISENGAGE);
+        for(int angle = NO_BREAK; angle > BREAK; angle-=15)    
+        {                                
+          Break.write(angle);           
+          delay(100);       
+        } 
         delay(1000);
-
+        for(int angle = BREAK; angle < NO_BREAK; angle+=15)  
+        {                                  
+          Break.write(angle);               
+          delay(100);                   
+        }
         ph_shift.write(NORMAL);
         throttle=1410;
         reverse = false;
       }
 
-
     }
   }
 
 }
-
-
-
-// void bbslowdown(int throttle) {
-//   for(; throttle>=1410; throttle)
-// }
