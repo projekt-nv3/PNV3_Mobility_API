e7d36c7d1243dafd303ee1dfbb42ad118f89db53
Elevenv <vitthaljadhav1420@gmail.com>
2023-06-01 20:32:24 +0530

    tty ports automation added

--- serialComm.py
+++ serialComm.py
@@ -1,9 +1,14 @@
 import serial
 import time
+import serial.tools.list_ports
+
+all_ports = serial.tools.list_ports.comports()
+avl_port = str(all_ports[0].device)
+# print(avl_port)
 
 arduino = serial.Serial(
-        port = "/dev/ttyACM0",
-        baudrate = 9600,
+        port = avl_port,
+        baudrate = 115200,
         bytesize = serial.EIGHTBITS, 
         parity = serial.PARITY_NONE,
         stopbits = serial.STOPBITS_ONE, 
@@ -14,22 +19,27 @@ arduino = serial.Serial(
         writeTimeout = 2
 )
 arduino.flush()
-# n = 0
-# string = ""
+
 while True:
     try:
         
         arduino.flush()
         # string = str(n)+"\n"
         # arduino.write(string.encode())
-        n = input()
-        arduino.write(n.encode())
-        data = arduino.readline()
-        if data:
-            print(data)
+
+        api = input("Throttle/Break/Steer/Reverse: ")
+        # Break = input("Break: ")
+        # reverse = input("Reverse: ")
+        # steer = input("Steer: ")
+        # l = [throttle,Break,reverse,steer]
+        # parameters_string = ','.join(str(x) for x in l)
+        # arduino.write(parameters_string.encode())
+        arduino.write(api.encode())
+        # data = arduino.readline()
+        # if data:
+        #     print(data)
         time.sleep(1)
     except Exception as e:
         print(e)
         arduino.close
     arduino.flush()
-    # n+=5
