4f8cf68af651d18aebd102ab8f319001a602c568
Elevenv <vitthaljadhav1420@gmail.com>
2023-06-01 20:31:44 +0530

    final changes updated

--- PNV3_Mobility_API.ino
+++ PNV3_Mobility_API.ino
@@ -7,8 +7,8 @@
 #define BREAK 95
 #define STEERL 40
 #define STEERR 140
+#define CAL 1020
 
-// defines variables
 long duration;
 int distance;
 bool reverse = false;
@@ -19,9 +19,8 @@ Servo esc;
 Servo Break;
 Servo Steer;
 
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   shift.attach(10); 
   shift.write(NORMAL);
   
@@ -31,12 +30,12 @@ void setup() {
   Break.write(NO_BREAK);
  
   esc.attach(9);
-  esc.writeMicroseconds(1400);
+  delay(2000);
+  esc.writeMicroseconds(1000);
   delay(2000);
 }
 
 void loop() {
-
   bool stop = false;
   if (Serial.available()) {
     String api_data = Serial.readStringUntil('\n');
@@ -46,6 +45,115 @@ void loop() {
       char first_char = api_data[0];
       
       Serial.print(first_char);
+
+        if(first_char=='T'){
+
+        
+          int th = data.toInt();
+          // speedUp(th);
+          Serial.println("THROTTLE");
+          for(int throttle=CAL; throttle<=th; throttle++) {
+            esc.writeMicroseconds(throttle);
+            delay(100);
+          }
+        }
+
+        else if(first_char=='B'){
+          Serial.println("BREAK");
+          esc.writeMicroseconds(0);
+          shift.write(DISENGAGE);
+          Break.write(BREAK);
+          delay(2000);
+          Break.write(NO_BREAK);
+          shift.write(NORMAL);
+          speedUp(CAL);
+        }
+
+        else if(first_char=='R'){
+          Serial.println("REVERSE");
+          if(!reverse) {
+            esc.writeMicroseconds(0);
+            shift.write(DISENGAGE);
+            Break.write(BREAK);
+            delay(1000);
+
+            Break.write(NO_BREAK);
+            shift.write(REVERSE);
+            reverse = true;
+            speedUp(CAL);
+          }
+      
+          else {
+            esc.writeMicroseconds(0);
+            delay(400);
+            shift.write(DISENGAGE);
+            Break.write(BREAK);
+            delay(1000);
+
+            Break.write(NO_BREAK);
+            shift.write(NORMAL);
+            reverse = false;
+            speedUp(CAL);
+          }
+
+        }
+
+        else if(first_char=='S'){
+          Serial.println("STEERING");
+          int a = data.toInt();
+          // if(data == 'L'){
+          //   int i;
+          //   for(i=steer_angle;i>40;i--){
+          //     Steer.write(i);
+          //     delay(100);
+          //   }
+          //   steer_angle = i;
+          // }
+          // else{
+          //   int j;
+          //   for(j=steer_angle;j<140;j++){
+          //     Steer.write(j);
+          //     delay(100);
+          //   }
+          //   steer_angle = j;
+          // }
+          // for(int j=90;j<140;j++){
+          //     Steer.write(j);
+          //     delay(100);
+          //   }
+          Steer.write(a);
+          delay(500);
+          // delay(2000);
+
+        }
+
+        else
+          Serial.println("Invalid first character.");
+    }
+  }
+}
+
+
+void speedUp(int th){
+  for(int throttle=CAL; throttle<=th; throttle++) {
+            esc.writeMicroseconds(throttle);
+            delay(100);
+          }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       // switch (first_char) {
       //   case 'T':
@@ -127,95 +235,3 @@ void loop() {
       //     Serial.println("Invalid first character.");
       //     break;
       // }
-
-        if(first_char=='T'){
-
-        
-          int th = data.toInt();
-          // speedUp(th);
-          Serial.println("THROTTLE");
-          for(int throttle=1410; throttle<=th; throttle++) {
-            esc.writeMicroseconds(throttle);
-            delay(100);
-          }
-        }
-
-        else if(first_char=='B'){
-          Serial.println("BREAK");
-          esc.writeMicroseconds(0);
-          shift.write(DISENGAGE);
-          Break.write(BREAK);
-          delay(2000);
-          Break.write(NO_BREAK);
-          shift.write(NORMAL);
-        }
-
-        else if(first_char=='R'){
-          Serial.println("REVERSE");
-          if(!reverse) {
-            esc.writeMicroseconds(0);
-            shift.write(DISENGAGE);
-            Break.write(BREAK);
-            delay(1000);
-
-            Break.write(NO_BREAK);
-            shift.write(REVERSE);
-            reverse = true;
-            speedUp(1430);
-          }
-      
-          else {
-            esc.writeMicroseconds(0);
-            delay(400);
-            shift.write(DISENGAGE);
-            Break.write(BREAK);
-            delay(1000);
-            Break.write(NO_BREAK);
-            shift.write(NORMAL);
-            reverse = false;
-            speedUp(1430);
-          }
-
-        }
-
-        else if(first_char=='S'){
-          Serial.println("STEERING");
-          int a = data.toInt();
-          // if(data == 'L'){
-          //   int i;
-          //   for(i=steer_angle;i>40;i--){
-          //     Steer.write(i);
-          //     delay(100);
-          //   }
-          //   steer_angle = i;
-          // }
-          // else{
-          //   int j;
-          //   for(j=steer_angle;j<140;j++){
-          //     Steer.write(j);
-          //     delay(100);
-          //   }
-          //   steer_angle = j;
-          // }
-          // for(int j=90;j<140;j++){
-          //     Steer.write(j);
-          //     delay(100);
-          //   }
-          Steer.write(a);
-          delay(500);
-
-        }
-
-        else
-          Serial.println("Invalid first character.");
-    }
-  }
-}
-
-
-void speedUp(int th){
-  for(int throttle=1410; throttle<=th; throttle++) {
-            esc.writeMicroseconds(throttle);
-            delay(100);
-          }
-}
